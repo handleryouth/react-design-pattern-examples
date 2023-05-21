df472c377050702f323586ed36e8bf6b4fef99e4
Tony <rafaeltonydavid@yahoo.com>
2023-05-21 15:14:36 +0700

    refactor: renitialize started code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
 import "./App.css";
-import SplitScreen from "./SplitScreen";
-
-export function LeftHandComponent({ name }: { name: string }) {
-  return (
-    <div
-      style={{
-        backgroundColor: "green",
-      }}
-    >
-      {name}
-    </div>
-  );
-}
-
-export function RightHandComponent({ message }: { message: string }) {
-  return (
-    <div
-      style={{
-        backgroundColor: "red",
-      }}
-    >
-      {message}
-    </div>
-  );
-}
 
 function App() {
-  return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name="John" />
-      <RightHandComponent message="Hello, world!" />
-    </SplitScreen>
-  );
+  return <div></div>;
 }
 
 export default App;
